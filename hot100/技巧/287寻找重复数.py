273ac991d4b050008fba78280a062b83e2693de9
andy <zcao0018@student.monash.edu>
2025-04-04 15:29:40 +1100

    add O(n) solution for 287

--- hot100/技巧/287寻找重复数.py
+++ hot100/技巧/287寻找重复数.py
@@ -2,13 +2,47 @@ from typing import *
 
 
 """
+O(n) time O(1) space，记这个！
+和41题第一个缺失的整数思路一样：
+    我们规定数组下标i存放数i + 1，即0放1，1放2，...
+    遍历数组每个下标i（n + 1所有位置都遍历），如果：
+        当前元素(nums[i]) 应当放到的那个下标处(nums[i] - 1) 不是当前元素，则把当前元素放到它应该放到的位置上（swap）。
+    最后，数组的最后一个元素就一定是重复数。
+    
+重要：
+为什么while条件不能判断当前的下标i上元素不是i + 1，即 while nums[i] != i + 1
+原因和41题类似，可能出现无穷循环，例如：
+    [3, 1, 3, 4, 2]
+在i = 1时，判断nums[i] != i + 1，即nums[0] != 1，我们就会把当前元素放到它应该放到的下标上，然后期待换过来的就是1。
+但是，当前的3应该放到下标2处，但下标2处恰好还是3，也就是重复数，这样最后就会造成两个3一直交换！
+所以：
+    我们改成判断当前元素应当放到的下标是不是已经是当前元素了，即nums[nums[i] - 1] != nums[i]，如果是就跳过。
+    
+等我们遍历完n + 1个元素后，我们能确保[1, n]这n个数一定在下标[0, n - 1]上，所以最后一个一定就是重复数。
+"""
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # 使用传统swap逻辑更清楚，同时不会因为pythonic写法造成问题！
+        def swap(i, j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+
+        n = len(nums)
+        for i in range(n):
+            while nums[nums[i] - 1] != nums[i]:
+                swap(nums[i] - 1, i)
+        return nums[-1]
+
+
+"""
 二分：O(1)额外空间，O(nlogn)时间
 原理：因为数字范围是[1, n]，那么对于某个数字x，如果整个数组中 <=x 的数字的个数大于x，则说明数字范围[1,x]中有重复数字，否则[x+1,n]中有重复数字。
 例如：[2, 3, 1, 2, 4]，对于数字2，小于等于2的有1,2,2，一共三个，如果2不重复则为1,2两个。此时就说明数字范围[1,2]中出现重复数字了。
 所以我们就可以二分搜索，统一左开右闭，数字的范围是[1, n]所以left = 1，right=len(nums)，也就是right = n + 1。注意我们二分是直接分数字[1,n]，
 不是下标！
 """
-class Solution:
+class Solution1:
     def findDuplicate(self, nums: List[int]) -> int:
         left, right = 1, len(nums)
         while left < right:
@@ -31,7 +65,7 @@ class Solution:
 下标代表抽屉：放之前（修改nums[i]之前），先把nums[i]的数存下来作为下一个要放的数，然后再标记nums[i] = 0，意味着抽屉i已经放过了。因为数的范围是[1, n]，
 所以抽屉0没用，同时我们应该先从nums中第一个数开始，即下标0，所以一开始我们从0开始（虽然一开始给抽屉0存放多余，但是统一了逻辑）。
 """
-class Solution1:
+class Solution2:
     def findDuplicate(self, nums: List[int]) -> int:
         i = 0
         while True:
