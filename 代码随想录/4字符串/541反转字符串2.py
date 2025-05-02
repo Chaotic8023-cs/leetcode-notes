9aa0667ba6aedef56942042d177d7ec3ce64cdf7
andy <zcao0018@student.monash.edu>
2025-05-02 14:09:48 +1000

    优化反转字符串2的写法

--- 代码随想录/4字符串/541反转字符串2.py
+++ 代码随想录/4字符串/541反转字符串2.py
@@ -6,24 +6,23 @@ class Solution:
     把string转成list做会方便很多，操作按题意即可。
     """
     def reverseStr(self, s: str, k: int) -> str:
-        def reverse(i, j, arr):
-            left, right = i, j
-            while left < right:
-                arr[left], arr[right] = arr[right], arr[left]
-                left, right = left + 1, right - 1
+        def reverse(l, r):
+            while l < r:
+                s[l], s[r] = s[r], s[l]
+                l, r = l + 1, r - 1
 
+        s = list(s)  # 转成list
         n = len(s)
-        arr = list(s)
-        start = 0
-        while start + 2 * k - 1 <= n:  # 当前有2k个
-            reverse(start, start + k - 1, arr)
-            start += 2 * k
-        #当剩余的长度n - start不足2k时，按题意分情况
-        if n - start < k:
-            reverse(start, n - 1, arr)
-        else:
-            reverse(start, start + k - 1, arr)
-        return ''.join(arr)
+        i = 0
+        while i + 2 * k < n:  # 当前有2k个字母则一直反转前k个
+            reverse(i, i + k - 1)
+            i += 2 * k
+        # 当剩余的字母个数n - i不足k时，全部反转
+        if n - i < k:
+            reverse(i, n - 1)
+        else:  # 否则，反转前k个
+            reverse(i, i + k - 1)
+        return ''.join(s)
 
 
 
