6ea1e4f47c31e2ae83e7d86a81f064c9f0dbfe1d
andy <zcao0018@student.monash.edu>
2025-05-04 16:08:21 +1000

    503下一个更大元素 加入wrap index的写法

--- 代码随想录/11单调栈/503下一个更大元素2.py
+++ 代码随想录/11单调栈/503下一个更大元素2.py
@@ -18,3 +18,35 @@ class Solution:
                 ans[prev_i] = arr[i]
             mStack.append(i)
         return ans[:n]
+
+class Solution1:
+    """
+    也可以使用wrap index：mstack中存两倍nums的下标，即[0, 2n - 1]，记录的时候对n求余即可
+    """
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        mstack = deque()  # 记录下标
+        ans = [-1] * n  # ans只用记录前n个，因为我们一直对i求余
+        for i in range(2 * n):
+            while mstack and nums[i % n] > nums[mstack[-1] % n]:
+                prev_i = mstack.pop()
+                ans[prev_i % n] = nums[i % n]
+            mstack.append(i)
+        return ans
+
+    """
+    当然，ans也可以初始化成2n长度的，然后仅在访问nums时求余，记录到ans中时仍使用2倍的下标
+    """
+    def nextGreaterElements1(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        mstack = deque()
+        ans = [-1] * 2 * n  # 记录2倍nums的下标
+        for i in range(2 * n):
+            while mstack and nums[i % n] > nums[mstack[-1] % n]:
+                prev_i = mstack.pop()
+                ans[prev_i] = nums[i % n]  # 直接记录长度为2倍nums的下标
+            mstack.append(i)
+        return ans[:n]  # 最后只取前n个
+
+
+
