41e116b9ca439820afb2820c4a2dfff9f1752950
Andy <Chaotic8023@gmail.com>
2025-06-20 10:44:28 +0800

    update #312 comments

--- 额外/312戳气球_区间dp.py
+++ 额外/312戳气球_区间dp.py
@@ -14,7 +14,7 @@ from typing import *
     即：对于排列[i, ..., k, ..., j]，开区间(i, j)上，我们假设下标k为最后一个被戳爆的气球，那么(i, j)上最高得分为
         k两侧的区间的最高得分，即dp[i][k]和dp[k][j]，加上最后被戳爆的气球k的得分，即nums[i] * nums[k] * nums[j]
         
-补充：为什么区间长度 length 是 [2, n - 1]？
+补充：为什么区间长度 length 是 [2, n - 1]（注意，range(2, n)中n取不到）？
 例如 nums = [3, 1, 5, 8]，在两端补了两个1后变成 [1, 3, 1, 5, 8, 1]，我们最后求的是开区间(0, n - 1)，此时n是6，包含了两端的1，所以开区间
 包含了原nums的所有数！于是，最长的区间长度就是 n-1，因为left的起始位置是0，加上 n-1=5 后区间刚好就是最大的 (0, 5)，并没有越界！
 """
@@ -26,7 +26,7 @@ class Solution:
         # 遍历区间长度：指left + 多少能得到right。最短为2，即只有一个元素
         for length in range(2, n):
             # 遍历区间左端点
-            for left in range(0, n - length):
+            for left in range(n - length):
                 right = left + length  # 根据左端点及区间长度，得到右端点
                 # 遍历最后一个戳破的位置k
                 for k in range(left + 1, right):  # 戳气球的位置是开区间内部
