ea5e730c5601a5dbf26a2409c1399f78cd5cb210
Andy <Chaotic8023@gmail.com>
2025-06-26 11:02:08 +0800

    update #53最大子数组和 要求返回子数组 优化

--- hot100/普通数组/53最大子数组和.py
+++ hot100/普通数组/53最大子数组和.py
@@ -37,5 +37,21 @@ class Solution1:
         return nums[max_idx - max_l + 1:max_idx + 1]  # 最后我们知道最大子数组出现的位置（结尾，即max_idx），和长度，于是往前推就能得到最大的子数组了
 
 
+# 上面Solution1的简化版：初始化时直接每个位置初始化成 nums[i]，这样遍历的时候就不用else专门赋值了；同时，不用单独记录max_l，因为最后可以直接从 dp[max_idx][1] 取得
+class Solution2:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        # 观察普通dp的递推公式，这里我们直接给每个位置的最大和初始化成 nums[i]，长度初始化为1，这样只需要进行 dp[i - 1][0] + nums[i] > nums[i] 一次比较！
+        dp = [[nums[i], 1] for i in range(n)]  # [以i为结尾的最大子数组和，以i为结尾的最大和子数组的长度]
+        max_sum, max_idx = nums[0], 0
+        for i in range(1, n):
+            if dp[i - 1][0] + nums[i] > nums[i]:
+                dp[i][0] = dp[i - 1][0] + nums[i]
+                dp[i][1] = dp[i - 1][1] + 1
+            if dp[i][0] > max_sum:
+                max_sum = dp[i][0]
+                max_idx = i
+        max_len = dp[max_idx][1]
+        return sum(nums[max_idx - max_len + 1:max_idx + 1])  # 拥有最大和的子数组就是 nums[max_idx - max_len + 1:max_idx + 1]
 
 
