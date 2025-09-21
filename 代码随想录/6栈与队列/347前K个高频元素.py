cb164485d1fdafce848794daefe89a17c9169e24
Andy <Chaotic8023@gmail.com>
2025-09-21 14:05:45 +0800

    udpate #347 前K个高频元素 优化写法

--- 代码随想录/6栈与队列/347前K个高频元素.py
+++ 代码随想录/6栈与队列/347前K个高频元素.py
@@ -9,13 +9,10 @@ class Solution:
     最小堆用来存储最大的那K个元素，当大小超过K时，就会把小的pop出去，最后只留下最大的那K个在堆里。
     """
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        pq = []
-        mp = Counter(nums)
-        for num, cnt in mp.items():
-            heapq.heappush(pq, (cnt, num))  # heapq会使用iterable中的第一个元素作为key
-            if len(pq) > k:
-                heapq.heappop(pq)
-        ans = []
-        while pq:
-            ans.append(heapq.heappop(pq)[1])
-        return ans
+        cnt = Counter(nums)
+        min_heap = []
+        for v, f in cnt.items():
+            heapq.heappush(min_heap, (f, v))
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        return [v for f, v in min_heap][::-1]  # min_heap是从小到大，因为要TopK，所以反转一下。（虽然题目要求以任意顺序返回，但是这里取反一下最终返回的就是按频率倒排）
