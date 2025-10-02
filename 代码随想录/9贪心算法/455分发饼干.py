96df849f9a6a567f78aad808d9a23d10988a8903
Andy <Chaotic8023@gmail.com>
2025-10-02 11:28:37 +0800

    update #455分发饼干 优化写法

--- 代码随想录/9贪心算法/455分发饼干.py
+++ 代码随想录/9贪心算法/455分发饼干.py
@@ -6,17 +6,16 @@ class Solution:
     自己做的思路：先都排序，然后双指针，饼干和胃口从小的开始，能分配就分配，分配不了饼干大小（j）就就+1
     """
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        m, n = len(g), len(s)
         g.sort()
         s.sort()
-        ans = 0
         i, j = 0, 0
-        while i < len(g) and j < len(s):
+        while i < m and j < n:
             if s[j] >= g[i]:
-                ans += 1
                 i, j = i + 1, j + 1
             else:
                 j += 1
-        return ans
+        return i  # i最后就是分发了几块饼干
 
 
 
