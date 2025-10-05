cb913612bd3c82c62153da1d6c106b5dcf6dde21
Andy <Chaotic8023@gmail.com>
2025-10-05 12:54:01 +0800

    update #123买卖股票的最佳时机3 简化写法

--- 代码随想录/10动态规划/123买卖股票的最佳时机3.py
+++ 代码随想录/10动态规划/123买卖股票的最佳时机3.py
@@ -35,6 +35,20 @@ class Solution:
             dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] - prices[i])  # 1. 昨天就第二次持有；2. 今天第二次买入，昨天第一次不持有
             dp[i][4] = max(dp[i - 1][4], dp[i - 1][3] + prices[i])  # 1. 昨天就第二次不持有；2. 今天第二次卖出，昨天第二次持有
         return max(dp[n - 1])  # 返回最后一天5中状态中的最大值
+    
+
+# 省略“还未操作”版本，dp数组直接从“第一次持有开始”
+class Solution1:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[0] * 4 for _ in range(n)]
+        dp[0][0], dp[0][1], dp[0][2], dp[0][3] = -prices[0], 0, -prices[0], 0
+        for i in range(1, n):
+            dp[i][0] = max(dp[i - 1][0], -prices[i])  # “还未操作” + 购入花费prices[i]，由于还未操作余额为0，所以直接就是 -prices[i]
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+            dp[i][2] = max(dp[i - 1][2], dp[i - 1][1] - prices[i])
+            dp[i][3] = max(dp[i - 1][3], dp[i - 1][2] + prices[i])
+        return max(dp[n - 1])
 
 
 
