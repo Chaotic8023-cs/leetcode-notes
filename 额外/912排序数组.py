9ffa3f2f0cd2d1502110ef4ce08db621118dcba9
Andy <Chaotic8023@gmail.com>
2025-10-20 15:16:35 +0800

    update #912排序数组 加入归并排序

--- 额外/912排序数组.py
+++ 额外/912排序数组.py
@@ -38,3 +38,65 @@ class Solution:
         return nums
 
 
+
+# 归并排序（切片）
+class Solution1:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge(l1, l2):
+            m, n = len(l1), len(l2)
+            ans = []
+            i, j = 0, 0
+            while i < m and j < n:
+                if l1[i] < l2[j]:
+                    ans.append(l1[i])
+                    i += 1
+                else:
+                    ans.append(l2[j])
+                    j += 1
+            if i < m:
+                ans.extend(l1[i:])
+            elif j < n:
+                ans.extend(l2[j:])
+            return ans
+        
+        def mergesort(nums):
+            if len(nums) <= 1:
+                return nums
+            mid = len(nums) // 2
+            l1_sorted = mergesort(nums[:mid])
+            l2_sorted = mergesort(nums[mid:])
+            return merge(l1_sorted, l2_sorted)
+
+        return mergesort(nums)
+
+
+# 归并排序（索引）
+class Solution2:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge(l1, l2):  # 和切片版一致
+            m, n = len(l1), len(l2)
+            ans = []
+            i, j = 0, 0
+            while i < m and j < n:
+                if l1[i] < l2[j]:
+                    ans.append(l1[i])
+                    i += 1
+                else:
+                    ans.append(l2[j])
+                    j += 1
+            if i < m:
+                ans.extend(l1[i:])
+            elif j < n:
+                ans.extend(l2[j:])
+            return ans
+
+        # 索引版，性能好
+        def mergesort(nums, left, right):  # [left, right) 左闭右开，同二分（都是闭区间的话不好处理）
+            if right - left <= 1:  # 实际上只会是 == 1（只有一个元素），不会到出现 left == right的情况
+                return nums[left:right]
+            mid = (left + right) >> 1
+            l1 = mergesort(nums, left, mid)
+            l2 = mergesort(nums, mid, right)
+            return merge(l1, l2)
+
+        return mergesort(nums, 0, len(nums))
