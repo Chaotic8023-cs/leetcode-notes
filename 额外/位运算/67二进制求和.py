4f5f3e6ab35305bd12662cd1d605c90972f8fb3c
Andy <Chaotic8023@gmail.com>
2025-10-21 11:14:36 +0800

    update #67 优化写法

--- 额外/位运算/67二进制求和.py
+++ 额外/位运算/67二进制求和.py
@@ -3,23 +3,24 @@ from typing import *
 
 """
 从右往左加，每次算当前位的结果和进位即可：结果对2求余，进位对2求商
-"""
+"""    
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        a, b = a[::-1], b[::-1]  # 为方便从右往左算，先反转一下
         m, n = len(a), len(b)
+        ans = ""
         carry = 0
-        i, j = m - 1, n - 1
-        ans = []
-        while i >= 0 or j >= 0:  # 只要其中有一个数还有就继续加，当另一个位空时设成0就行
-            da = int(a[i]) if i >= 0 else 0
-            db = int(b[j]) if j >= 0 else 0
-            res = da + db + carry
-            d, carry = res % 2, res // 2  # d：当前位置应当留的结果，carry：进位
-            ans.append(str(d))
-            i, j = i - 1, j - 1
-        if carry > 0:  # 最后如果还有进位则说明还得多一位
-            ans.append(str(carry))
-        return ''.join(ans)[::-1]  # 因为ans是从右往左记录的，所以最后翻转
+        i, j = 0, 0
+        while i < m or j < n:  # 只要其中有一个数还有就继续加，当另一个位空时设成0就行
+            da = int(a[i]) if i < m else 0
+            db = int(b[j]) if j < n else 0
+            d = da + db + carry
+            v, carry = d % 2, d // 2
+            ans += str(v)
+            i, j = i + 1, j + 1
+        if carry:  # 最后如果还有进位则说明还得多一位
+            ans += str(carry)
+        return ans[::-1]  # 因为ans是从右往左记录的，所以最后翻转
 
 
 
