8ee9ce6fdcd6a8f167c769200ccaa79c9749a3e5
Andy <Chaotic8023@gmail.com>
2025-11-10 13:06:13 +0800

    udpate #236 加入简单版本

--- hot100/二叉树/236二叉树的最近公共祖先.py
+++ hot100/二叉树/236二叉树的最近公共祖先.py
@@ -31,6 +31,33 @@ class Solution:
                 return seenP, seenQ, None
 
         return traverse(root)[2]
+    
+
+
+class Solution1:
+    """
+    最简单易记的版本：
+    traverse返回：
+        1. 在没找到LCA之前：以root为根节点的子树中找到的p或q（没找到就是None）
+        2. 在找到LCA后：返回LCA
+    此代码也恰好解决了p或q本身就是LCA的情况。
+    """
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def traverse(root):
+            if root is None:
+                return None
+            # 如果当前节点就是p/q，那么就返回找到的p/q
+            if root == p or root == q:
+                return root
+            l = traverse(root.left)
+            r = traverse(root.right)
+            # 如果pq分别在左右子树中找到，则当前root为LCA，直接返回LCA
+            if l is not None and r is not None:
+                return root
+            # 在没找到LCA前：返回左右子树中找到p或q；在找到LCA后，返回的就是LCA
+            return l or r
+
+        return traverse(root)
 
 
 
