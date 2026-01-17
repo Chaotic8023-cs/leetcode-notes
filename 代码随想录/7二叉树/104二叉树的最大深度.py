341cba4ecf9a563ab632c7c63b6f9f24391a8289
Andy <chaotic8023@gmail.com>
2026-01-17 13:19:22 +0800

    update #104

--- 代码随想录/7二叉树/104二叉树的最大深度.py
+++ 代码随想录/7二叉树/104二叉树的最大深度.py
@@ -8,15 +8,17 @@ class TreeNode:
         self.left = left
         self.right = right
 
-
 class Solution:
     """
     递归：当前节点的最大深度等于左右子树中的最大深度+1，相当于后序遍历
     """
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        def traverse(root):
+            if root is None:
+                return 0
+            l = traverse(root.left)
+            r = traverse(root.right)
+            return max(l, r) + 1
 
-
+        return traverse(root)
 
