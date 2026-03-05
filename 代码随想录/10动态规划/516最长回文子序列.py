524dd3dc73c6d618201f2fe5dcd8d974b39e5900
Andy <Chaotic8023@gmail.com>
2026-03-05 12:57:05 +0800

    update #516

--- 代码随想录/10动态规划/516最长回文子序列.py
+++ 代码随想录/10动态规划/516最长回文子序列.py
@@ -55,6 +55,21 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
         return dp[0][n - 1]
+    
+    def longestPalindromeSubseq2(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                # 这么分情况也行
+                if s[i] == s[j]:
+                    if j - i < 2:
+                        dp[i][j] = j - i + 1
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][n - 1]
 
 
 
