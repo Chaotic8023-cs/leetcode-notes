d4d78704ceb26482c2942edf9c0c38a791ce24ff
Andy <chaotic8023@gmail.com>
2026-03-30 15:35:45 +1100

    add #395

---
+++ 额外/395至少有 K 个重复字符的最长子串.py
@@ -0,0 +1,20 @@
+from typing import *
+
+
+
+"""
+分治：非法字符分割
+
+若字符 c 在字符串中出现次数 < k
+则任何包含 c 的子串都不可能合法
+用 c 将字符串分割，递归求解每一段
+"""
+class Solution:
+    def longestSubstring(self, s: str, k: int) -> int:
+        if not s:  # base case
+            return 0
+        for c in set(s):
+            if s.count(c) < k:  # 找到不合法字符（即总数都小于k）
+                # 对当前合法的子段递归计算
+                return max(self.longestSubstring(sub, k) for sub in s.split(c))
+        return len(s)
