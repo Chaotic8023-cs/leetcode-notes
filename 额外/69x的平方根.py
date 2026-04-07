4b3ebcce2970b8ee75b5a9533cff603093339550
Andy <chaotic8023@gmail.com>
2026-04-07 13:21:35 +1000

    update #69

--- 额外/69x的平方根.py
+++ 额外/69x的平方根.py
@@ -6,16 +6,15 @@ from typing import *
 """
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x < 2:
+        if x < 2:  # 处理x=1的情况
             return x
-        # 根号x不会超过x // 2（因为舍弃小数部分），所以+1使得右区间为开区间
-        left, right = 1, x // 2 + 1
+        left, right = 0, x // 2 + 1
         while left < right:
             mid = (left + right) >> 1
-            if mid * mid > x:
-                right = mid
-            else:  # mid * mid <= x
+            if mid ** 2 <= x:
                 left = mid + 1
+            else:
+                right = mid
         """
         循环结束的前一个循环中，一定走的是left = mid + 1，此时mid * mid <= x，那么left = mid + 1就是第一个平方大于x的数
         所以最后返回left - 1！
